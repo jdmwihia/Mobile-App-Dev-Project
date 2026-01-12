f014794add7d42cdb12e10ab5b459db83e0020e2
Jeremy Mwihia <jeremwihia@gmail.com>
2026-01-12 17:36:16 +0300

    BMI Calc 2

---
+++ app/src/main/java/com/example/bmicalc2/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.bmicalc2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bmicalc2.ui.theme.BMICalc2Theme
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BMICalc2Theme {
+        Greeting("Android")
+    }
+}
